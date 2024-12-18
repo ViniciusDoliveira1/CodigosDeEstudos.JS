dbfebd2a551cacfb0b291a0d01fd11c1caf213a0
vinicius de oliveira <156321428+ViniciusDoliveira1@users.noreply.github.com>
2024-12-18 13:56:49 -0300

    Add files via upload

---
+++ first class function.js
@@ -0,0 +1,18 @@
+// first class function- exemplo usando uma calculadora de 4 metodos
+
+
+const add= function(a,b) {
+return a+b
+}
+const subtract = function (a, b) {
+return a - b
+}
+
+const multiply= (x, y)=> x * y
+const divide= (x, y)=> x / y
+
+
+console.log(add(10,20))
+console.log(subtract(10,20))
+console.log(multiply(10,20))
+console.log(divide(10,20))
